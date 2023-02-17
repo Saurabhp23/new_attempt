edeb94686d10110a0e3a5be4b925a139d1c5c704
Saurabh <saurabhpaloha@gmail.com>
2023-02-17 13:05:19 +0530

    commited new code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
       @user = User.find(params[:id])
   end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :about))
+    @user.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index, :new, :create]
 end
